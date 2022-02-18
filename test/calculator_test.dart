eec3065983541df2c1fb9d6815e7394c2beb0826
benbright065@gmail.com <benbright065@gmail.com>
2022-02-18 14:40:53 +0100

    unit test

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -1,7 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:test_project/calculator.dart';
 
 void main(){
-test("the calculator returns 4 when adding 2 and 2", {
-
+test("the calculator returns 4 when adding 2 and 2", (){
+  final calculator = Calculator();
+  expect(calculator.add(2, 2), 33);
 });
 }
+
+
+
+
